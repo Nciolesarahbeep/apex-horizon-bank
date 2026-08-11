fe0f14293bfa2e9dc730737b7ef93dc09ad697f8
ogbonnemafemi001-beep <ogbonnemafemi001@gmail.com>
2026-08-11 22:04:55 +0100

    Await user session retrieval in transactions handler

--- api/transactions.js
+++ api/transactions.js
@@ -10,7 +10,7 @@ module.exports = async function handler(req, res) {
   }
 
   try {
-    const session = getUserFromRequest(req);
+    const session = await getUserFromRequest(req);
     if (!session) {
       return res.status(401).json({ error: 'Not authenticated' });
     }
